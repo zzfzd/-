7565776c6de819998ce4477fb81bf1c7dea974ab
zzfzd <130893288+zzfzd@users.noreply.github.com>
2025-07-19 16:41:18 +0000

    1

--- 实验/实例4.cpp
+++ 实验/实例4.cpp
@@ -3,21 +3,21 @@ int isleapyear(int year)
 {
     if(year%4==0&&year%100!=0||year%400==0)//闰年判断规则
     {
-        ruturn 1//ruturn会立即终止函数的执行
+        return 1;//ruturn会立即终止函数的执行
     }
-    return 0
+    return 0;
 }
 int main()
 {
     int year,month,day;
-    int daysinmonth[]={31,28,31,30,31,30,31,31,30,31,30,31}
+    int daysinmonth[]={31,28,31,30,31,30,31,31,30,31,30,31};//数组第一个数据的位置是0
     int today=0;
     printf("请输入年月日");
     scanf("%d %d %d",&year,&month,&day);
-    for(int i=1;i<month;i++)
+    for(int i=0;i<month-1;i++)
     {
         today=today+daysinmonth[i];
         
     }
-    printf("%d",today=today+day);
+    printf("%d",today=today+day+(month>2&&isleapyear(year)));
 }
