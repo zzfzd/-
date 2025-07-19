97f601a5f4214602312b8b95931a1533b2e84c36
zzfzd <130893288+zzfzd@users.noreply.github.com>
2025-07-19 22:17:07 +0000

    4

--- 实验/实例8.cpp
+++ 实验/实例8.cpp
@@ -1,2 +1,13 @@
-shanzzsssazzzzz
-zzz
+#include<stdio.h>
+int main()
+{
+int i,j,k;
+for(i=1;i<10;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        printf("%d*%d ",i,j);
+    }
+    printf("\n");
+}
+}
