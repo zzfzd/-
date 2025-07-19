8eebfcbafed037fdb80baeea82a6c937575d156e
zzfzd <130893288+zzfzd@users.noreply.github.com>
2025-07-19 18:35:01 +0000

    1

--- 实验/实例5.cpp
+++ 实验/实例5.cpp
@@ -5,16 +5,22 @@ int main()
     printf("请输入三个数");
     scanf("%f%f%f",&x,&y,&z);
     if(x>y)
+    {
     t=x;
     x=y;
     y=t;
+    }
     if(x>z)
+    {
     t=x;
     x=z;
     z=t;
+    }
     if(y>z)
+    {
     t=y;
     y=z;
     z=t;
+    }
     printf("%f%f%f",x,y,z);
 }
